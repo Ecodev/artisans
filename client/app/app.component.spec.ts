4eddb93732ba5179aeac4a4187c973bcf1bc5984
Adrien Crivelli <adrien.crivelli@gmail.com>
2018-11-08 18:14:47 +1100

    Configure headless unit tests

--- client/app/app.component.spec.ts
+++ client/app/app.component.spec.ts
@@ -1,12 +1,25 @@
 import { async, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { ApolloModule } from 'apollo-angular';
+import { NgProgressModule } from '@ngx-progressbar/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatCheckboxModule, MatDialogModule, MatSnackBarModule } from '@angular/material';
 
 describe('AppComponent', () => {
     beforeEach(async(() => {
         TestBed.configureTestingModule({
             imports: [
                 RouterTestingModule,
+                NgProgressModule.forRoot(),
+                ApolloModule,
+                BrowserModule,
+                BrowserAnimationsModule,
+                MatButtonModule,
+                MatCheckboxModule,
+                MatDialogModule,
+                MatSnackBarModule,
             ],
             declarations: [
                 AppComponent,

eccea08e1b01618a2e958a6ff7a696403c238117
Samuel Baptista <samuel.baptista@ecodev.ch>
2018-11-13 19:39:53 +0100

    Fix travis

--- client/app/app.component.spec.ts
+++ client/app/app.component.spec.ts
@@ -1,5 +1,4 @@
 import { async, TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { ApolloModule } from 'apollo-angular';
 import { NgProgressModule } from '@ngx-progressbar/core';
@@ -7,23 +6,66 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MockApolloProvider } from './shared/testing/MockApolloProvider';
 import { AppMaterialModule } from './app-material/app-material.module';
+import { RelationsComponent } from './shared/components/relations/relations.component';
+import { AppRoutingModule } from './app-routing.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { IconModule } from './shared/components/icon/icon.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SelectComponent } from './shared/components/select/select.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import {
+    DateAdapter,
+    ErrorStateMatcher,
+    MAT_FORM_FIELD_DEFAULT_OPTIONS,
+    MatIconRegistry,
+    ShowOnDirtyErrorStateMatcher,
+} from '@angular/material';
+import { TimezonePreservingDateAdapter } from './shared/services/timezone.preserving.date.adapter';
+import { APP_BASE_HREF } from '@angular/common';
 
 describe('AppComponent', () => {
     beforeEach(async(() => {
         TestBed.configureTestingModule({
-            imports: [
-                RouterTestingModule,
-                NgProgressModule.forRoot(),
-                ApolloModule,
-                BrowserModule,
-                BrowserAnimationsModule,
-                AppMaterialModule,
-            ],
             declarations: [
                 AppComponent,
+                RelationsComponent,
+                SelectComponent,
+            ],
+            imports: [
+                BrowserModule,
+                BrowserAnimationsModule,
+                RouterTestingModule,
+                FormsModule,
+                ReactiveFormsModule,
+                NgProgressModule.forRoot(),
+                ApolloModule,
+                AppRoutingModule,
+                FlexLayoutModule,
+                AppMaterialModule,
+                IconModule,
             ],
             providers: [
                 MockApolloProvider,
+                MatIconRegistry,
+                {
+                    provide: DateAdapter,
+                    useClass: TimezonePreservingDateAdapter,
+                },
+                {
+                    // Use OnDirty instead of default OnTouched, that allows to validate while editing. Touched is updated after blur.
+                    provide: ErrorStateMatcher,
+                    useClass: ShowOnDirtyErrorStateMatcher,
+                },
+                {
+                    provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
+                    useValue: {
+                        appearance: 'fill',
+                    },
+                },
+                {
+                    provide: APP_BASE_HREF,
+                    useValue: '/',
+                },
             ],
         }).compileComponents();
     }));

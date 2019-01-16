663e8364695341fd598d57b047f5e7d07530492c
Samuel Baptista <samuel.baptista@ecodev.ch>
2019-01-16 14:41:09 +0100

    Drop 'inactive' role from database #6067

--- server/Application/Migration/Version20190115160500.php
+++ server/Application/Migration/Version20190115160500.php
@@ -13,5 +13,6 @@ class Version20190115160500 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE user ADD status ENUM(\'inactive\', \'active\', \'new\', \'archived\') DEFAULT \'new\' NOT NULL COMMENT \'(DC2Type:UserStatus)\'');
+        $this->addSql('ALTER TABLE user CHANGE role role ENUM(\'booking_only\', \'member\', \'responsible\', \'administrator\') DEFAULT \'member\' NOT NULL COMMENT \'(DC2Type:UserRole)\'');
     }
 }

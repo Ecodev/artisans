67c955ea0c76bd4ad0d3d9ce7fd8bf489272884f
Adrien Crivelli <adrien.crivelli@gmail.com>
2019-01-25 22:25:11 +1100

    Fix Travis

--- server/Application/Migration/Version20190125093753.php
+++ server/Application/Migration/Version20190125093753.php
@@ -14,6 +14,8 @@ class Version20190125093753 extends AbstractMigration
 
         $this->addSql('ALTER TABLE expense_claim DROP FOREIGN KEY FK_461791DA76ED395');
         $this->addSql('DROP INDEX IDX_461791DA76ED395 ON expense_claim');
+        $this->addSql('ALTER TABLE expense_claim DROP FOREIGN KEY FK_461791D7E3C61F9');
         $this->addSql('ALTER TABLE expense_claim DROP user_id, CHANGE owner_id owner_id INT NOT NULL');
+        $this->addSql('ALTER TABLE expense_claim ADD CONSTRAINT FK_461791D7E3C61F9 FOREIGN KEY (owner_id) REFERENCES user (id)');
     }
 }

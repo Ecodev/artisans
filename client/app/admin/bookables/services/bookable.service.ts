f80ce7049305af4474248549501935700a1de2ec
Samuel Baptista <samuel.baptista@ecodev.ch>
2019-01-29 13:47:08 +0100

    Add utility functions to BookableService and resolve with empty user instead of null #6110

--- client/app/admin/bookables/services/bookable.service.ts
+++ client/app/admin/bookables/services/bookable.service.ts
@@ -15,17 +15,22 @@ import {
     BookablesQuery,
     BookablesQueryVariables,
     BookableState,
+    BookingsQueryVariables,
     BookingType,
     CreateBookableMutation,
     CreateBookableMutationVariables,
+    CurrentUserForProfileQuery,
     DeleteBookablesMutation,
     UpdateBookableMutation,
     UpdateBookableMutationVariables,
+    UserQuery,
 } from '../../../shared/generated-types';
 import { Validators } from '@angular/forms';
 import { Observable, of } from 'rxjs';
 import { QueryVariablesManager } from '../../../shared/classes/query-variables-manager';
 import { map } from 'rxjs/operators';
+import { BookingService } from '../../bookings/services/booking.service';
+import { intersectionBy } from 'lodash';
 
 @Injectable({
     providedIn: 'root',
@@ -59,7 +64,7 @@ export class BookableService extends AbstractModelService<BookableQuery['bookabl
         filter: {groups: [{conditions: [{bookingType: {in: {values: [BookingType.admin_approved]}}}]}]},
     };
 
-    constructor(apollo: Apollo) {
+    constructor(apollo: Apollo, private bookingService: BookingService) {
         super(apollo,
             'bookable',
             bookableQuery,
@@ -88,6 +93,21 @@ export class BookableService extends AbstractModelService<BookableQuery['bookabl
                 ],
             },
         };
+    }
+
+    public static isLicenceGranted(bookable: BookableQuery['bookable'],
+                                   user: UserQuery['user'] | CurrentUserForProfileQuery['viewer']): boolean {
+
+        if (!bookable || !user) {
+            return false;
+        }
+
+        // If no constraints on bookable, there is not need for licence
+        if (bookable.licenses.length === 0) {
+            return true;
+        }
+
+        return intersectionBy(bookable.licenses, user.licenses, 'id').length;
     }
 
     public getEmptyObject(): BookableInput {
@@ -125,6 +145,40 @@ export class BookableService extends AbstractModelService<BookableQuery['bookabl
         return this.getAll(qvm);
     }
 
+    public canBook(bookable: BookableQuery['bookable'],
+                   user: UserQuery['user'] | CurrentUserForProfileQuery['viewer']): Observable<boolean> {
+        return this.isAvailable(bookable).pipe(map(isAvailable => isAvailable && BookableService.isLicenceGranted(bookable, user)));
+    }
+
+    public isAvailable(bookable: BookableQuery['bookable']): Observable<boolean> {
+
+        if (!bookable.isActive) {
+            return of(false);
+        }
+
+        // Variable for pending bookings related to given bookable
+        const variables: BookingsQueryVariables = {
+            filter: {
+                groups: [
+                    {
+                        conditions: [
+                            {
+                                bookables: {have: {values: [bookable.id]}},
+                                endDate: {null: {}},
+                            },
+                        ],
+                    },
+                ],
+            },
+        };
+
+        const qvm = new QueryVariablesManager<BookingsQueryVariables>();
+        qvm.set('variables', variables);
+        return this.bookingService.getAll(qvm).pipe(map(result => {
+            return bookable.simultaneousBookingMaximum > result.length;
+        }));
+    }
+
     public resolveByCode(code: string): Observable<{ model: any }> {
 
         if (code) {
@@ -135,7 +189,7 @@ export class BookableService extends AbstractModelService<BookableQuery['bookabl
             qvm.set('variables', variables);
 
             return this.getAll(qvm).pipe(map(result => {
-                return {model: result && result.items.length ? result.items[0] : null};
+                return {model: result && result.items.length ? result.items[0] : {}};
             }));
         } else {
             return of({model: null});

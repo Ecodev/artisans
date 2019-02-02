339bcd88ff5b6f93015ea4277ae1d86913791d35
Adrien Crivelli <adrien.crivelli@gmail.com>
2019-02-02 16:23:22 +1100

    Add global configuration with safe values
    This is to avoid access to non-existing keys if we didn't set local
    values in `local.php` yet.

---
+++ config/autoload/global.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Default configuration that can be overridden by a local.php, but it at least ensure
+ * that required keys exist with "safe" values.
+ */
+return [
+    'emailOverride' => null,
+    'smtp' => null,
+    'doorsApi' => [
+        'endpoint' => 'http://localhost:8888',
+        'token' => 'my-token-value',
+        'authorizedIps' => ['localhost'],
+    ],
+];

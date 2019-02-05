54b8dcb539711ab7dc9bc9e480c88234fde383be
Samuel Baptista <samuel.baptista@ecodev.ch>
2019-02-05 15:34:23 +0100

    Better declaration of big buttons and use ichtus logo as icon in header

--- client/app/dashboard/dashboard.component.ts
+++ client/app/dashboard/dashboard.component.ts
@@ -1,31 +1,18 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { UserService } from '../admin/users/services/user.service';
 import { BookingService } from '../admin/bookings/services/booking.service';
-import { AutoRefetchQueryRef } from '../shared/services/abstract-model.service';
-import { BookingsQuery } from '../shared/generated-types';
-import { animate, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
     selector: 'app-dashboard',
     templateUrl: './dashboard.component.html',
     styleUrls: ['./dashboard.component.scss'],
-    animations: [
-        trigger('terminate', [
-            state('end', style({transform: 'scale(1, 1)'})),
-            transition(':leave', [
-                animate('0.4s ease-in-out', style({transform: 'scale(0, 0)'})),
-            ]),
-        ]),
-    ],
+
 })
-export class DashboardComponent implements OnInit, OnDestroy {
+export class DashboardComponent implements OnInit {
 
     public title = 'my-ichtus';
 
     public currentUser;
-
-    public userRunningBookings;
-    public userRunningBookingsQueryRef: AutoRefetchQueryRef<BookingsQuery['bookings']>;
 
     constructor(public userService: UserService, public bookingService: BookingService) {
     }
@@ -33,18 +20,7 @@ export class DashboardComponent implements OnInit, OnDestroy {
     public ngOnInit(): void {
         this.userService.getCurrentUser().subscribe(user => {
             this.currentUser = user;
-            this.userRunningBookingsQueryRef = this.userService.getRunningNavigations(user);
-            this.userRunningBookingsQueryRef.valueChanges.subscribe(bookings => {
-                this.userRunningBookings = bookings.items;
-            });
         });
-
-    }
-
-    ngOnDestroy() {
-        if (this.userRunningBookingsQueryRef) {
-            this.userRunningBookingsQueryRef.unsubscribe();
-        }
     }
 
     public canAccessAdmin() {
@@ -53,11 +29,6 @@ export class DashboardComponent implements OnInit, OnDestroy {
 
     public canAccessDoor() {
         return UserService.canAccessDoor(this.currentUser);
-    }
-
-    public endBooking(booking, index) {
-        this.bookingService.flagEndDate(booking.id);
-        this.userRunningBookings.splice(index, 1);
     }
 
 }

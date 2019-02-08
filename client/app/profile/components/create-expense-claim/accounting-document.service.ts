4bbf5ba8f0ac9459fca19ccdd400e92da72701f8
Samuel Baptista <samuel.baptista@ecodev.ch>
2019-02-08 14:55:36 +0100

    Add  AccountingDocument service #6111

---
+++ client/app/profile/components/create-expense-claim/accounting-document.service.ts
@@ -0,0 +1,55 @@
+import { Injectable } from '@angular/core';
+import { Apollo } from 'apollo-angular';
+import { AbstractModelService } from '../../../shared/services/abstract-model.service';
+
+import gql from 'graphql-tag';
+import { userMetaFragment } from '../../../shared/queries/fragments';
+import {
+    AccountingDocumentInput,
+    CreateAccountingDocumentMutation,
+    CreateAccountingDocumentMutationVariables,
+} from '../../../shared/generated-types';
+
+export const createAccountingDocumentMutation = gql`
+    mutation CreateAccountingDocument($input: AccountingDocumentInput!) {
+        createAccountingDocument(input: $input) {
+            id
+            creator {
+                ...userMeta
+            }
+        }
+    }
+    ${userMetaFragment}
+`;
+
+@Injectable({
+    providedIn: 'root',
+})
+export class AccountingDocumentService extends AbstractModelService<any,
+    any,
+    any,
+    any,
+    CreateAccountingDocumentMutation['createAccountingDocument'],
+    CreateAccountingDocumentMutationVariables,
+    any,
+    any,
+    any> {
+
+    constructor(apollo: Apollo) {
+        super(apollo,
+            'image',
+            null,
+            null,
+            createAccountingDocumentMutation,
+            null,
+            null);
+    }
+
+    public getEmptyObject(): AccountingDocumentInput {
+        return {
+            file: '',
+            expenseClaim: '',
+        };
+    }
+
+}

6e8d2c8269a08e36f7543c52720a6834b9d5ce1c
Adrien Crivelli <adrien.crivelli@gmail.com>
2019-03-25 23:30:41 +1400

    Update permissions

--- configuration/Permissions.php
+++ configuration/Permissions.php
@@ -4,17 +4,17 @@ declare(strict_types=1);
 
 return [
     [
-        'path' => 'bin/dump-schema',
-        'permissions' => '0750',
+        'path' => 'data/accounting',
+        'permissions' => '0770',
+        'recursive' => false,
+    ],
+    [
+        'path' => 'data/images',
+        'permissions' => '0770',
         'recursive' => false,
     ],
     [
         'path' => 'logs',
-        'permissions' => '0770',
-        'recursive' => true,
-    ],
-    [
-        'path' => 'data/dump',
         'permissions' => '0770',
         'recursive' => true,
     ],
@@ -29,16 +29,6 @@ return [
         'recursive' => true,
     ],
     [
-        'path' => 'data/accounting',
-        'permissions' => '0770',
-        'recursive' => false,
-    ],
-    [
-        'path' => 'data/images',
-        'permissions' => '0770',
-        'recursive' => false,
-    ],
-    [
         'path' => 'bin/build.sh',
         'permissions' => '0750',
     ],
@@ -47,7 +37,23 @@ return [
         'permissions' => '0750',
     ],
     [
+        'path' => 'bin/create-triggers.php',
+        'permissions' => '0750',
+    ],
+    [
+        'path' => 'bin/cron.php',
+        'permissions' => '0750',
+    ],
+    [
         'path' => 'bin/dump-data.php',
+        'permissions' => '0750',
+    ],
+    [
+        'path' => 'bin/dump-schema',
+        'permissions' => '0750',
+    ],
+    [
+        'path' => 'bin/graphql.php',
         'permissions' => '0750',
     ],
     [
@@ -63,23 +69,15 @@ return [
         'permissions' => '0750',
     ],
     [
-        'path' => 'node_modules/.bin/*',
-        'permissions' => '0750',
-    ],
-    [
         'path' => 'bin/pre-commit.sh',
         'permissions' => '0750',
     ],
     [
-        'path' => 'link-libraries.sh',
+        'path' => 'bin/send-message.php',
         'permissions' => '0750',
     ],
     [
-        'path' => 'bin/graphql.php',
-        'permissions' => '0750',
-    ],
-    [
-        'path' => 'bin/create-triggers.php',
+        'path' => 'node_modules/.bin/*',
         'permissions' => '0750',
     ],
 ];

c4d4b2ebc57f5b871ba6b9f1f359d7f1eb9edd4a
Sylvain Tissot <sylvain.tissot@ecodev.ch>
2019-03-29 14:33:12 +0100

    Migrate user.billingType = 'all_electronic' to 'electronic'

--- server/Application/Migration/Version20190329113311.php
+++ server/Application/Migration/Version20190329113311.php
@@ -12,6 +12,8 @@ class Version20190329113311 extends AbstractMigration
     {
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
+        $this->addSql('ALTER TABLE user CHANGE billing_type billing_type ENUM(\'all_electronic\', \'paper_bill_electronic_reminder\', \'paper_bill_paper_reminder\', \'electronic\', \'paper\') DEFAULT \'electronic\' NOT NULL COMMENT \'(DC2Type:BillingType)\'');
+        $this->addSql('UPDATE user SET billing_type=\'electronic\' WHERE billing_type = \'all_electronic\'');
         $this->addSql('ALTER TABLE user CHANGE billing_type billing_type ENUM(\'electronic\', \'paper\') DEFAULT \'electronic\' NOT NULL COMMENT \'(DC2Type:BillingType)\'');
     }
 }

4607f292ddcd40a0f2dc6e3b3ffaf74715df0b3a
Adrien Crivelli <adrien.crivelli@gmail.com>
2019-04-05 15:51:52 +1400

    Fix unit tests

--- tests/data/query/individual-can-total-bookables.php
+++ tests/data/query/individual-can-total-bookables.php
@@ -17,7 +17,7 @@ return [
             'bookables' => [
                 'totalPurchasePrice' => '33347.00',
                 'totalInitialPrice' => '10.00',
-                'totalPeriodicPrice' => '130.00',
+                'totalPeriodicPrice' => '140.00',
             ],
         ],
     ],

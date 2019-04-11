b2d0fe6b2f0fc90c2e20962aec4993182f67fc08
Adrien Crivelli <adrien.crivelli@gmail.com>
2019-04-11 20:56:19 +1200

    Don't send email for negative balance in January #6221

--- bin/queue-message-for-negative-balance.php
+++ bin/queue-message-for-negative-balance.php
@@ -2,8 +2,16 @@
 <?php
 
 use Application\Service\MessageQueuer;
+use Cake\Chronos\Date;
 
 $container = require_once 'server/cli.php';
+
+$currentMonth = Date::today()->format('m');
+if ($currentMonth === '01') {
+    echo 'Emails for accounts with negative balances were not queued because it is January.' . PHP_EOL;
+    echo 'Instead an email to everybody should be queued manually via `./bin/queue-message-for-all-balance.php`.' . PHP_EOL;
+    exit(1);
+}
 
 /** @var MessageQueuer $messageQueuer */
 $messageQueuer = $container->get(MessageQueuer::class);

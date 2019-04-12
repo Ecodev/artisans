0c4ac25f9353595efc7a0e14a64c940eaf6a57fb
Sylvain Tissot <sylvain.tissot@ecodev.ch>
2019-04-12 15:52:30 +0200

    Check that every transaction is balanced #6234

--- bin/check-accounting.php
+++ bin/check-accounting.php
@@ -6,6 +6,7 @@
  */
 use Application\DBAL\Types\AccountTypeType;
 use Application\Model\Account;
+use Application\Model\Transaction;
 
 require_once 'server/cli.php';
 
@@ -30,6 +31,29 @@ Passifs   : ' . $liabilities . '
 Capital   : ' . $equity . '
 Écart     : ' . $discrepancy . PHP_EOL;
 
+$errors = [];
+
 if (bccomp($discrepancy, '0.00') !== 0) {
+    $errors[] = sprintf('ERREUR: écart de %s au bilan des comptes!', $discrepancy);
+}
+
+foreach (_em()->getRepository(Transaction::class)->findAll() as $transaction) {
+    $totalDebit = '0.00';
+    $totalCredit = '0.00';
+    foreach ($transaction->getTransactionLines() as $line) {
+        if ($line->getDebit()) {
+            $totalDebit = bcadd($totalDebit, $line->getBalance());
+        }
+        if ($line->getCredit()) {
+            $totalCredit = bcadd($totalCredit, $line->getBalance());
+        }
+    }
+    if (bccomp($totalDebit, $totalCredit) !== 0) {
+        $errors[] = sprintf('ERREUR: transaction %u non-équilibrée, débit: %s, crédit: %s', $transaction->getId(), $totalDebit, $totalCredit);
+    }
+}
+
+if (count($errors)) {
+    echo PHP_EOL . implode(PHP_EOL, $errors) . PHP_EOL . PHP_EOL;
     exit(1);
 }

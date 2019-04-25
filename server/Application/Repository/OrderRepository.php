3d0445f7cb793a97b99b9d0bba4b70d308c8eca8
Adrien Crivelli <adrien.crivelli@gmail.com>
2019-04-25 21:53:30 +1200

    Remove obsolete code

--- server/Application/Repository/OrderRepository.php
+++ server/Application/Repository/OrderRepository.php
@@ -4,10 +4,6 @@ declare(strict_types=1);
 
 namespace Application\Repository;
 
-use Application\Api\Exception;
-use Application\Api\Helper;
-use Application\Model\Order;
-use Application\Model\OrderLine;
 use Application\Model\User;
 
 class OrderRepository extends AbstractRepository implements LimitedAccessSubQueryInterface
@@ -33,46 +29,5 @@ class OrderRepository extends AbstractRepository implements LimitedAccessSubQuer
               JOIN order_line ON order.id = order_line.order_id
               JOIN account ON order_line.debit_id = account.id OR order_line.credit_id = account.id 
               WHERE account.owner_id = ' . $user->getId();
-    }
-
-    /**
-     * @param Order $order
-     * @param array $lines
-     */
-    public function hydrateLinesAndFlush(Order $order, array $lines): void
-    {
-        if (!$lines) {
-            throw new Exception('A Order must have at least one OrderLine');
-        }
-
-        // Destroy all previously existing OrderLine
-        foreach ($order->getOrderLines() as $line) {
-            $this->getEntityManager()->remove($line);
-        }
-        $order->getOrderLines()->clear();
-
-        $accounts = [];
-        foreach ($lines as $line) {
-            $orderLine = new OrderLine();
-            Helper::hydrate($orderLine, $line);
-            if (!$orderLine->getCredit() && !$orderLine->getDebit()) {
-                throw new Exception('Cannot create a OrderLine without any account');
-            }
-            $accounts[] = $orderLine->getCredit();
-            $accounts[] = $orderLine->getDebit();
-
-            $orderLine->setOrder($order);
-            $orderLine->setOrderDate($order->getOrderDate());
-            $this->getEntityManager()->persist($orderLine);
-        }
-
-        $this->getEntityManager()->persist($order);
-        $this->getEntityManager()->flush();
-
-        // Be sure to refresh the new account balance that were computed by DB triggers
-        $accounts = array_filter(array_unique($accounts, SORT_REGULAR));
-        foreach ($accounts as $account) {
-            $this->getEntityManager()->refresh($account);
-        }
     }
 }

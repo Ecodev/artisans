e9a0e873441c5ec69a37db34e2e8ed7a5ba3cc73
Adrien Crivelli <adrien.crivelli@gmail.com>
2019-05-06 15:55:28 +1200

    Delete obsolete importer

--- configuration/Permissions.php
+++ configuration/Permissions.php
@@ -41,10 +41,6 @@ return [
         'permissions' => '0750',
     ],
     [
-        'path' => 'bin/import-products.php',
-        'permissions' => '0750',
-    ],
-    [
         'path' => 'bin/send-all-messages.php',
         'permissions' => '0750',
     ],

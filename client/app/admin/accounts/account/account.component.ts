b6c5a8012f7c6400809bf0b934916828e8b60974
Sylvain Tissot <sylvain.tissot@ecodev.ch>
2019-08-05 14:55:41 +0200

    Add missing fields to account detail view #6619

--- client/app/admin/accounts/account/account.component.ts
+++ client/app/admin/accounts/account/account.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { NaturalAbstractDetail } from '@ecodev/natural';
 import { NaturalAlertService } from '@ecodev/natural';
@@ -12,6 +12,7 @@ import {
     UpdateAccountVariables,
 } from '../../../shared/generated-types';
 import { UserService } from '../../users/services/user.service';
+import { GroupAccountHierarchicConfiguration } from '../../../shared/hierarchic-selector/GroupAccountHierarchicConfiguration';
 
 @Component({
     selector: 'app-account',
@@ -25,14 +26,60 @@ export class AccountComponent
         CreateAccountVariables,
         UpdateAccount['updateAccount'],
         UpdateAccountVariables,
-        any> {
+        any> implements OnInit {
+
+    public nextCodeAvailable: number;
+    public accountHierarchicConfig = GroupAccountHierarchicConfiguration;
 
     constructor(alertService: NaturalAlertService,
-                accountService: AccountService,
+                public accountService: AccountService,
                 router: Router,
                 route: ActivatedRoute,
                 public userService: UserService,
     ) {
         super('account', accountService, alertService, router, route);
+    }
+
+    ngOnInit(): void {
+        super.ngOnInit();
+        this.accountService.getNextCodeAvailable().subscribe(code => {
+            this.nextCodeAvailable = code;
+            if (!this.data.model.id) {
+                const codeField = this.form.get('code');
+                if (codeField) {
+                    codeField.setValue(code);
+                }
+            }
+        });
+        const parentId = this.route.snapshot.params['parent'];
+        if (parentId) {
+            this.accountService.getOne(parentId).subscribe(parentAccount => {
+                const parentField = this.form.get('parent');
+                if (parentAccount.id && parentField) {
+                    parentField.setValue({id: parentAccount.id, name: [parentAccount.code, parentAccount.name].join(' ')});
+                }
+            });
+        }
+    }
+    public getAccountLabel(account: Account['account']): string {
+        if (account) {
+            return [account.code, account.name].join(' ');
+        }
+        return '';
+    }
+    public updateLinkedFields(): void {
+        const typeField = this.form.get('type');
+        if (typeField && typeField.value !== 'liability') {
+            const ownerField = this.form.get('owner');
+            if (ownerField) {
+                ownerField.setValue(null);
+            }
+        }
+        if (typeField && typeField.value !== 'asset') {
+            const ibanField = this.form.get('iban');
+            if (ibanField) {
+                ibanField.setValue('');
+            }
+        }
     }
 }

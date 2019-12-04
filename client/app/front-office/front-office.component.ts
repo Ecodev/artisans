f36adcb9ce92096e7350eef6102d9c7fddc74086
Samuel Baptista <samuel.baptista@ecodev.ch>
2019-12-04 01:37:18 +0100

    Navigation and menus (including mobile)

--- client/app/front-office/front-office.component.ts
+++ client/app/front-office/front-office.component.ts
@@ -1,7 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 import { NaturalAbstractController, NaturalAlertService } from '@ecodev/natural';
+import { differenceBy } from 'lodash';
 import { CurrentUserForProfile_viewer, UserRole } from '../shared/generated-types';
+
+interface MenuItem {
+    display: string;
+    children?: MenuItem[];
+    link?: RouterLink['routerLink'];
+}
 
 @Component({
     selector: 'app-front-office',
@@ -16,6 +23,64 @@ export class FrontOfficeComponent extends NaturalAbstractController implements O
     public UserRole = UserRole;
     public viewer: CurrentUserForProfile_viewer;
 
+    /**
+     * In case of change, check CSS dimensions :
+     * li.opened > ul {max-height: 400px} // mobile menu for transition
+     * .hasMenu { height: 300px} // white background on mega menu
+     */
+    public navigation: MenuItem[] = [
+        {
+            display: 'L\'association',
+            link: '/association',
+        },
+        {
+            display: 'La revue durable',
+            link: '/la-revue',
+        },
+        {
+            display: 'Agir avec nous',
+            link: 'agir-avec-nous',
+            children: [
+                {display: 'Les conversations carbonne'},
+                {display: 'Les conversations carbonne'},
+                {
+                    display: 'Les conversations carbonne',
+                    children: [
+                        {display: 'Les conversations carbonne'},
+                        {display: 'Les conversations carbonne'},
+                        {display: 'Les conversations carbonne'},
+                    ],
+                },
+                {display: 'Les conversations carbonne'},
+            ],
+        },
+        {
+            display: 'Nous soutenir',
+            link: '/nous-soutenir',
+        },
+    ];
+
+    public topNavigation: MenuItem[] = [
+        {
+            display: 'Agenda',
+            link: '/evenements',
+        },
+        {
+            display: 'Nous contacter',
+            link: '/contact',
+        },
+        {
+            display: 'Panier',
+            link: '/panier',
+        },
+        {
+            display: 'Mon compte',
+            link: '/profile',
+        },
+    ];
+
+    public mobileNavigation: MenuItem[] = [];
+
     constructor(public route: ActivatedRoute,
                 public alertService: NaturalAlertService,
     ) {
@@ -23,7 +88,15 @@ export class FrontOfficeComponent extends NaturalAbstractController implements O
     }
 
     ngOnInit() {
-        this.viewer = this.route.snapshot.data.viewer.model;
+        this.viewer = this.route.snapshot.data.viewer.model as CurrentUserForProfile_viewer;
+
+        // Setup mobile menu with items from top menu that are missing on main menu
+        this.mobileNavigation = [...this.navigation, ...differenceBy(this.topNavigation, this.navigation, 'link')];
+
+        if (this.viewer && this.viewer.role === UserRole.administrator) {
+            this.topNavigation.splice(0, 0, {display: 'Administration', link: '/admin'});
+            this.mobileNavigation.push({display: 'Administration', link: '/admin'});
+        }
     }
 
     public search() {

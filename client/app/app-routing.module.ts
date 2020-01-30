3b209375f7baaff769df30ecf853810b0af91030
Samuel Baptista <samuel.baptista@ecodev.ch>
2020-01-30 23:05:35 +0100

    Prevent routing colision

--- client/app/app-routing.module.ts
+++ client/app/app-routing.module.ts
@@ -5,10 +5,6 @@ import { ErrorComponent } from './shared/components/error/error.component';
 
 export const routes: Routes = [
     {
-        path: '',
-        loadChildren: () => import('./front-office/front-office.module').then(m => m.FrontOfficeModule),
-    },
-    {
         path: 'admin',
         loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule),
     },
@@ -17,6 +13,10 @@ export const routes: Routes = [
         path: 'user',
         component: FrontOfficeComponent,
         loadChildren: () => import('./user/user.module').then(m => m.UserModule),
+    },
+    {
+        path: '',
+        loadChildren: () => import('./front-office/front-office.module').then(m => m.FrontOfficeModule),
     },
     {
         path: 'error',

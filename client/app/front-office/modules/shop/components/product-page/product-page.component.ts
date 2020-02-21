0d8d07d55f1ab7e4302daa599671ba04b480a445
Adrien Crivelli <adrien.crivelli@gmail.com>
2020-02-21 14:41:55 +0700

    Only show buy buttons if necessary #6873

--- client/app/front-office/modules/shop/components/product-page/product-page.component.ts
+++ client/app/front-office/modules/shop/components/product-page/product-page.component.ts
@@ -1,6 +1,6 @@
 import { Component, Injector, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { NaturalAbstractDetail } from '@ecodev/natural';
+import { NaturalAbstractDetail, NaturalQueryVariablesManager } from '@ecodev/natural';
 import { ProductService } from '../../../../../admin/products/services/product.service';
 import { getDownloadLink } from '../../../../../shared/components/file/file.component';
 import {
@@ -11,9 +11,11 @@ import {
     Product_product,
     ProductType,
     ProductVariables,
+    PurchasesVariables,
     UpdateProduct,
     UpdateProductVariables,
 } from '../../../../../shared/generated-types';
+import { PurchaseService } from '../../../../../profile/components/purchases/purchase.service';
 
 @Component({
     selector: 'app-product-page',
@@ -56,8 +58,14 @@ export class ProductPageComponent
      */
     public formGroup = new FormGroup({quantity: this.quantityForm});
     public viewer: CurrentUserForProfile['viewer'];
+    public showBuyPaper = false;
+    public showBuyDigital = false;
 
-    constructor(private productService: ProductService, injector: Injector) {
+    constructor(
+        private productService: ProductService,
+        private purchaseService: PurchaseService,
+        injector: Injector,
+    ) {
         super('product', productService, injector);
     }
 
@@ -65,6 +73,24 @@ export class ProductPageComponent
         super.ngOnInit();
 
         this.viewer = this.route.snapshot.data.viewer ? this.route.snapshot.data.viewer.model : null;
+
+        const qvm = new NaturalQueryVariablesManager<PurchasesVariables>();
+        qvm.set('variables', {
+            filter: {groups: [{conditions: [{id: {equal: {value: this.data.model.id}}}]}]},
+        });
+
+        // Show button to buy only if we didn't already bought those version
+        this.purchaseService.getAll(qvm).subscribe(purchases => {
+            const paper = [ProductType.both, ProductType.paper];
+            const digital = [ProductType.both, ProductType.digital];
+
+            this.showBuyPaper = paper.includes(this.data.model.type)
+                && !purchases.items.some(orderLine => paper.includes(orderLine.type));
+
+            this.showBuyDigital = digital.includes(this.data.model.type)
+                && !purchases.items.some(orderLine => digital.includes(orderLine.type))
+                && !this.data.model.file; // A digital might be allowed via subscription, not purchase
+        });
     }
 
     public getDownloadLink(product: Product_product): null | string {

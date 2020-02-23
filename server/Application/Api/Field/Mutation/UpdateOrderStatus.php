d6763c5a2561960eb021d62edf4e517b03605924
Adrien Crivelli <adrien.crivelli@gmail.com>
2020-02-23 15:46:39 +0700

    Notify people when order is validated in GUI #6874

--- server/Application/Api/Field/Mutation/UpdateOrderStatus.php
+++ server/Application/Api/Field/Mutation/UpdateOrderStatus.php
@@ -7,6 +7,10 @@ namespace Application\Api\Field\Mutation;
 use Application\Api\Enum\OrderStatusType;
 use Application\Api\Field\FieldInterface;
 use Application\Model\Order;
+use Application\Model\User;
+use Application\Repository\UserRepository;
+use Application\Service\Mailer;
+use Application\Service\MessageQueuer;
 use GraphQL\Type\Definition\Type;
 use Mezzio\Session\SessionInterface;
 
@@ -30,22 +34,29 @@ abstract class UpdateOrderStatus implements FieldInterface
                 $order = $args['id']->getEntity();
                 $order->setStatus($status);
 
-                // Todo : send notification to customer
-                //
-                //                if ($status === Order::STATUS_VALIDATED) {
-                //                    /** @var Mailer $mailer */
-                //                    $mailer = $container->get(Mailer::class);
-                //
-                //                    /** @var MessageQueuer $messageQueuer */
-                //                    $messageQueuer = $container->get(MessageQueuer::class);
-                //
-                //                    // Notify user
-                //                    $message = $messageQueuer->queueValidatedOrder($user, $user->getEmail());
-                //                    $mailer->sendMessageAsync($message);
-                //                }
-
                 _em()->flush();
                 _em()->refresh($order);
+
+                if ($status === Order::STATUS_VALIDATED) {
+                    /** @var Mailer $mailer */
+                    $mailer = $container->get(Mailer::class);
+
+                    /** @var MessageQueuer $messageQueuer */
+                    $messageQueuer = $container->get(MessageQueuer::class);
+
+                    // Notify user
+                    $message = $messageQueuer->queueUserValidatedOrder($order);
+                    $mailer->sendMessageAsync($message);
+
+                    // Notify admins
+                    /** @var UserRepository $repository */
+                    $repository = _em()->getRepository(User::class);
+                    $admins = $repository->getAllAdministratorsToNotify();
+                    foreach ($admins as $admin) {
+                        $message = $messageQueuer->queueAdminValidatedOrder($admin, $order);
+                        $mailer->sendMessageAsync($message);
+                    }
+                }
 
                 return $order;
             },

c0a0d58816d84851d9eaeca44b4ced7a325ca0fa
Sylvain Tissot <sylvain.tissot@ecodev.ch>
2020-02-24 23:33:03 +0100

    Protect cookies again cross site requests and prevent iframe inclusion #7050
    We use `Strict` because even hen coming from external link, the first HTTP
    request will load Angular which does not require any cookie and then graphql
    requests will be sent correctly with the cookie by the Angular app itself

--- config/container.php
+++ config/container.php
@@ -9,6 +9,7 @@ ini_set('session.cookie_secure', '1');
 ini_set('session.cookie_httponly', '1');
 ini_set('session.save_path', dirname(__DIR__) . '/data/session');
 ini_set('session.gc_maxlifetime', (string) (365 * 86400));
+ini_set('session.cookie_samesite', 'Strict');
 
 // Load configuration
 $config = require __DIR__ . '/config.php';

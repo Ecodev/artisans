0807c56794284fa254db99207404925e75a7f117
Samuel Baptista <samuel.baptista@ecodev.ch>
2020-04-28 18:44:35 +0200

    Subscriptions page

--- client/app/shared/components/price/price.component.ts
+++ client/app/shared/components/price/price.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { NaturalAbstractController } from '@ecodev/natural';
 import { takeUntil } from 'rxjs/operators';
 import { CartLineProduct } from '../../../front-office/modules/cart/classes/cart';
+import { Subscriptions_subscriptions_items } from '../../generated-types';
 import { Currency, CurrencyService } from '../../services/currency.service';
 
 @Component({
@@ -11,7 +12,7 @@ import { Currency, CurrencyService } from '../../services/currency.service';
 })
 export class PriceComponent extends NaturalAbstractController implements OnInit {
 
-    @Input() product: CartLineProduct;
+    @Input() product: CartLineProduct | Subscriptions_subscriptions_items;
 
     public price;
 

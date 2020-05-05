e7407974600db8c58bf44981fa8238e793162251
Adrien Crivelli <adrien.crivelli@gmail.com>
2020-05-05 11:56:49 +0900

    Properly refetch watchedQueries

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -112,7 +112,7 @@ export class AppModule {
             link: link,
             cache: new InMemoryCache(),
             defaultOptions: apolloDefaultOptions,
-            ssrMode: true,
+            ssrMode: !isBrowser,
         });
     }
 }

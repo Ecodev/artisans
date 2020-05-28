0d7d24271bd1863e99c984c17f8e2a681434adea
Adrien Crivelli <adrien.crivelli@gmail.com>
2020-05-28 09:11:57 +0900

    Delete cookie when logging out to serve via SSR

---
+++ config/autoload/session.global.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'session' => [
+        'persistence' => [
+            'ext' => [
+                'delete_cookie_on_empty_session' => true,
+            ],
+        ],
+    ],
+];

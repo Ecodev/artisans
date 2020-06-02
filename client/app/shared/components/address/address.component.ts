62534aa6b31238d8ef2d31331507f450a5a08a62
Adrien Crivelli <adrien.crivelli@gmail.com>
2020-06-02 09:30:02 +0900

    Drop unused code

--- client/app/shared/components/address/address.component.ts
+++ client/app/shared/components/address/address.component.ts
@@ -2,15 +2,6 @@ import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {FormGroup} from '@angular/forms';
 import {CountryService} from './country.service';
 
-export const markFormGroupTouched = formGroup => {
-    (Object as any).values(formGroup.controls).forEach(control => {
-        control.markAsTouched();
-        if (control.controls) {
-            markFormGroupTouched(control);
-        }
-    });
-};
-
 @Component({
     selector: 'app-address',
     templateUrl: './address.component.html',

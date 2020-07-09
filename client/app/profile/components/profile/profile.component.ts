bdf5814a10e5013b73e83a165e793727d64d0e83
Adrien Crivelli <adrien.crivelli@gmail.com>
2020-07-09 12:41:42 +0900

    New button to stop membership #7008

--- client/app/profile/components/profile/profile.component.ts
+++ client/app/profile/components/profile/profile.component.ts
@@ -2,7 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {NaturalAlertService} from '@ecodev/natural';
 import {UserService} from '../../../admin/users/services/user.service';
-import {CurrentUserForProfile, Product, ProductType} from '../../../shared/generated-types';
+import {CurrentUserForProfile, Membership, Product, ProductType} from '../../../shared/generated-types';
 import {ProductService} from '../../../admin/products/services/product.service';
 
 @Component({
@@ -13,6 +13,7 @@ import {ProductService} from '../../../admin/products/services/product.service';
 export class ProfileComponent implements OnInit {
     public viewer: CurrentUserForProfile['viewer'];
     public ProductType = ProductType;
+    public Membership = Membership;
 
     public membershipProduct: Product['product'];
 
@@ -26,5 +27,21 @@ export class ProfileComponent implements OnInit {
     public ngOnInit(): void {
         this.viewer = this.route.snapshot.data.viewer.model;
         this.productService.getMembershipProduct().subscribe(product => (this.membershipProduct = product));
+    }
+
+    public requestMembershipEnd(): void {
+        this.alertService
+            .confirm(
+                'Arrêter les cotisations',
+                'Voulez-vous envoyer une demande pour arrêter les cotisations aux Artisans de la transition ?',
+                'Envoyer',
+            )
+            .subscribe(confirmed => {
+                if (confirmed) {
+                    this.userService.requestMembershipEnd().subscribe(() => {
+                        this.alertService.info("Une demande d'arrêt des cotisation a été envoyée");
+                    });
+                }
+            });
     }
 }

8d8ae319ddc8798abe10d56502d18b9627249d6d
Samuel Baptista <samuel.baptista@ecodev.ch>
2020-09-14 18:26:51 +0200

    SEO description #7416

--- client/app/front-office/front-office-routing.module.ts
+++ client/app/front-office/front-office-routing.module.ts
@@ -89,22 +89,46 @@ const routes: Routes = [
             {
                 path: 'nos-convictions',
                 component: ConvictionsComponent,
-                data: {seo: {title: 'Nos convictions'} as SEO},
+                data: {
+                    seo: {
+                        title: 'Nos convictions',
+                        description:
+                            'La volonté de l’association Artisans de la transition est de créer un maximum d’opportunités pour aider à agir afin de créer une société soutenable.',
+                    } as SEO,
+                },
             },
             {
                 path: 'qui-sommes-nous',
                 component: QuiSommesNousComponent,
-                data: {seo: {title: 'Qui sommes-nous ?'} as SEO},
+                data: {
+                    seo: {
+                        title: 'Qui sommes-nous ?',
+                        description:
+                            'Une petite équipe motivée et compétente qui s’efforce chaque jour de mettre leurs actes en cohérence avec leurs idées et leurs convictions.',
+                    } as SEO,
+                },
             },
             {
                 path: 'partenariats',
                 component: PartenariatsComponent,
-                data: {seo: {title: 'Partenariats'} as SEO},
+                data: {
+                    seo: {
+                        title: 'Partenariats',
+                        description:
+                            'Les Artisans de la transition sont partenaires officiels de la Plateforme Développement durable des hautes écoles de la HES-SO. Elle est aussi partenaire du Cas développement durable de l’Université de Genève, de l’Hepia et de l’association Tera en France. ',
+                    } as SEO,
+                },
             },
             {
                 path: 'status',
                 component: ComiteStatusComponent,
-                data: {seo: {title: 'Status'} as SEO},
+                data: {
+                    seo: {
+                        title: 'Status',
+                        description:
+                            'Président : Yvan Maillard Ardenti pilote le programme « Justice climatique » à la fondation Pain pour le prochain. Secrétaire : Jacques Eschmann, géographe. Membres du comité : Daniel Chambaz, Pietro Majno, Aurianne Stroude et Berthe d’Arras.',
+                    } as SEO,
+                },
             },
         ],
     },
@@ -122,22 +146,46 @@ const routes: Routes = [
                     {
                         path: 'methode',
                         component: SessionMethodComponent,
-                        data: {seo: {title: 'Conversations carbone'} as SEO},
+                        data: {
+                            seo: {
+                                title: 'Conversations carbone',
+                                description:
+                                    'Depuis 2017, plus de 400 personnes ont participé à une Conversation carbone en Suisse romande. En moyenne, 80 % d’entre elles estiment que cette expérience les a motivées à modifier leur comportement, 70 % ont augmenté leurs connaissances.',
+                            } as SEO,
+                        },
                     },
                     {
                         path: 'facilitateurs',
                         component: SessionFacilitatorComponent,
-                        data: {seo: {title: 'Facilitateurs - Conversations carbone'} as SEO},
+                        data: {
+                            seo: {
+                                title: 'Facilitateurs - Conversations carbone',
+                                description:
+                                    'Si vous aimez le contact, faites preuve d’empathie et d’écoute non jugeante et avez envie de vous engager pour le climat, nous vous proposons de devenir facilitateur.trice de Conversations carbone. ',
+                            } as SEO,
+                        },
                     },
                     {
                         path: 'organisations',
                         component: SessionOrganisationComponent,
-                        data: {seo: {title: 'Organisateurs - Conversations carbone'} as SEO},
+                        data: {
+                            seo: {
+                                title: 'Organisateurs - Conversations carbone',
+                                description:
+                                    'Organiser une Conversations Carbone en entreprise ou association est tout à fait possible et même recommandable. ',
+                            } as SEO,
+                        },
                     },
                     {
                         path: 'prochaines',
                         component: SessionsIncomingComponent,
-                        data: {seo: {title: 'Prochaines sessions conversations carbone'} as SEO},
+                        data: {
+                            seo: {
+                                title: 'Prochaines sessions conversations carbone',
+                                description:
+                                    'Les Conversations carbone se tiennent régulièrement en Suisse romande. Si vous n’avez pas trouvé de session qui vous convienne, n’hésitez pas à vous inscrire sur l’une des listes d’attente régionales.',
+                            } as SEO,
+                        },
                     },
                     // {
                     //     path: ':sessionId',
@@ -149,42 +197,90 @@ const routes: Routes = [
             {
                 path: 'toutes-nos-actions',
                 component: ActionsComponent,
-                data: {seo: {title: 'Nos actions'} as SEO},
+                data: {
+                    seo: {
+                        title: 'Nos actions',
+                        description:
+                            'Les actions de l’association sont structurées en trois axes selon une logique progressive. On commence par s’informer, puis on se sent concerné au point de vouloir s’impliquer, et on se met à agir.',
+                    } as SEO,
+                },
             },
             {
                 path: 'calculer-empreinte-carbone',
                 component: CalculerEmpreinteCarboneComponent,
-                data: {seo: {title: 'Calculer son empreinte carbone'} as SEO},
+                data: {
+                    seo: {
+                        title: 'Calculer son empreinte carbone',
+                        description:
+                            'Calculer ses émissions personnelles de CO2 et comprendre où l’on se situe par rapport à la moyenne nationale et à la contrainte climatique est un premier pas dans l’engagement pour la préservation des équilibres climatiques.',
+                    } as SEO,
+                },
             },
             {
                 path: 'desinvestir-industrie-energies-fossiles',
                 component: DesinvestirFossileComponent,
-                data: {seo: {title: "Desinvestir de l'industrie des énergies fossiles"} as SEO},
+                data: {
+                    seo: {
+                        title: "Desinvestir de l'industrie des énergies fossiles",
+                        description:
+                            'Les Artisans de la transition animent la campagne de désinvestissement des énergies fossiles. Elle est particulièrement pertinente en Suisse, au 70e rang en termes d’émissions directes de CO2, au 7e rang en termes d’émissions générées par sa place financière.',
+                    } as SEO,
+                },
             },
             {
                 path: 'apero-divest',
                 component: AperoDivestComponent,
-                data: {seo: {title: 'Apero divest'} as SEO},
+                data: {
+                    seo: {
+                        title: 'Apero divest',
+                        description:
+                            'Le principe de l’Apéro-Divest est très simple : vous réunissez une dizaine de personnes en cercle sans rien au centre dans votre salon et faites appel à un ou deux facilitateurs ou facilitatrices formé-e-s par les Artisans de la transition pour assurer l’animation.',
+                    } as SEO,
+                },
             },
             {
                 path: 'rapport-bns',
                 component: BnsComponent,
-                data: {seo: {title: 'Rapports BNS'} as SEO},
+                data: {
+                    seo: {
+                        title: 'Rapports BNS',
+                        description:
+                            'Depuis 2016, l’association a publié trois rapports sur les investissements de la Banque Nationale Suisse dans l’industrie des énergies fossiles, dont les deux premiers ont donné lieu à de nombreuses motions parlementaires, y compris au niveau fédéral.',
+                    } as SEO,
+                },
             },
             {
                 path: 'numerique-ethique',
                 component: NumeriqueEthiqueComponent,
-                data: {seo: {title: 'Numérique éthique'} as SEO},
+                data: {
+                    seo: {
+                        title: 'Numérique éthique',
+                        description:
+                            'En janvier 2019, un livre extraordinaire est paru aux Etats-Unis. Son autrice, Shoshana Zuboff, y dévoile avec maestria la façon dont Google, puis Facebook, la Silicon Valley et enfin l’ensemble du tissu économique bouleversent le capitalisme depuis 2001.',
+                    } as SEO,
+                },
             },
             {
                 path: 'alimentation',
                 component: AlimentationComponent,
-                data: {seo: {title: 'Alimentation'} as SEO},
+                data: {
+                    seo: {
+                        title: 'Alimentation',
+                        description:
+                            'Les Artisans de la transition veulent dynamiser la transition agroécologique et paysanne en tissant des liens entre ces acteurs, en soutenant leurs actions, en augmentant leur nombre et leur poids politique pour, in fine, œuvrer à un changement du système alimentaire.',
+                    } as SEO,
+                },
             },
             {
                 path: 'agir-au-quotidien',
                 component: AgirAuQuotidienComponent,
-                data: {seo: {title: 'Agir au quotidien'} as SEO},
+                data: {
+                    seo: {
+                        title: 'Agir au quotidien',
+                        description:
+                            'Au gré des parutions de LaRevueDurable, ce site propose des pistes d’action au quotidien. Pour l’instant, nous vous proposons d’agir sur le numérique éthique et de capitaliser sur les changements de comportement que vous avez adopté pendant le confinement. ',
+                    } as SEO,
+                },
             },
         ],
     },
@@ -194,22 +290,46 @@ const routes: Routes = [
             {
                 path: 'faire-un-don',
                 component: FaireUnDonComponent,
-                data: {seo: {title: 'Faire un don'} as SEO},
+                data: {
+                    seo: {
+                        title: 'Faire un don',
+                        description:
+                            'Les dons composent la moitié des revenus de l’association. Ils garantissent son indépendance. En faisant un don à l’association, vous soutenez une petite équipe motivée et compétente.',
+                    } as SEO,
+                },
             },
             {
                 path: 'rejoindre-association',
                 component: RejoindreAssociationComponent,
-                data: {seo: {title: "Rejoindre l'association"} as SEO},
+                data: {
+                    seo: {
+                        title: "Rejoindre l'association",
+                        description:
+                            'Plus nous aurons de membres, plus l’élan sera fort et plus nous pourrons continuer d’identifier en toute indépendance les actions les plus pertinentes à entreprendre.',
+                    } as SEO,
+                },
             },
             {
                 path: 'offrir-la-revue-durable',
                 component: OffrirLaRevueComponent,
-                data: {seo: {title: "Offrir ou s'offrir la revue durable"} as SEO},
+                data: {
+                    seo: {
+                        title: "Offrir ou s'offrir la revue durable",
+                        description:
+                            'LaRevueDurable est le cœur battant du travail des Artisans de la transition. La source d’où jaillissent toutes leurs initiatives : Le Climat entre nos mains, les Conversations carbone, les Grands-parents pour le climat, la campagne pour désinvestir de l’industrie des énergies fossiles, le numérique éthique.',
+                    } as SEO,
+                },
             },
             {
                 path: 'nous-faire-connaitre',
                 component: NousFaireConnaitreComponent,
-                data: {seo: {title: 'Nous faire connaître'} as SEO},
+                data: {
+                    seo: {
+                        title: 'Nous faire connaître',
+                        description:
+                            'Vous pouvez mettre des flyers de notre association dans votre commerce ? Installer une « caisse qui parle » dans votre restaurant - une caisse à vin décorée et remplie d’anciens numéros de LaRevueDurable et des flyers en lien avec nos activités ?',
+                    } as SEO,
+                },
             },
         ],
     },
@@ -228,12 +348,24 @@ const routes: Routes = [
             {
                 path: 'projet',
                 component: ProjetComponent,
-                data: {seo: {title: 'Notre projet'} as SEO},
+                data: {
+                    seo: {
+                        title: 'Notre projet',
+                        description:
+                            'LaRevueDurable n’est pas un « produit » de presse, c’est une création, une quête permanente. Pour savoir si cette revue peut vous instruire et vous aider à avancer, y a-t-il une autre option que de lire et de découvrir ses numéros ?',
+                    } as SEO,
+                },
             },
             {
                 path: 'points-de-vente',
                 component: PointsDeVenteComponent,
-                data: {seo: {title: 'Points de vente'} as SEO},
+                data: {
+                    seo: {
+                        title: 'Points de vente',
+                        description:
+                            'Pour éviter le gaspillage lié à la vente en kioque, LaRevueDurable est vendue uniquement en librairie. Nous avons une quinzaine de points de vente en Suisse et en France. ',
+                    } as SEO,
+                },
             },
         ],
     },
--- client/app/front-office/modules/shop/shop-routing.module.ts
+++ client/app/front-office/modules/shop/shop-routing.module.ts
@@ -137,7 +137,11 @@ const routes: Routes = [
         path: 'abonnements',
         component: SubscriptionsComponent,
         data: {
-            seo: {title: 'Abonnements'},
+            seo: {
+                title: 'Abonnements',
+                description:
+                    'Plus de 1000 articles de fond sur tous les thèmes qui touchent à la durabilité dans une soixantaine de pays. LaRevueDurable constitue l’une des plus importantes base de données disponibles sur la durabilité et la transition écologique en français.',
+            },
             breadcrumbs: [{label: 'La Revue Durable'}],
         },
     },

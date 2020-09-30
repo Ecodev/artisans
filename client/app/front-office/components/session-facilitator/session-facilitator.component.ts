4b8ec587cca0513b9da73e2b8fbcfd2d5e9b6c02
Samuel Baptista <samuel.baptista@ecodev.ch>
2020-09-30 15:20:35 +0200

    Show facilitators

--- client/app/front-office/components/session-facilitator/session-facilitator.component.ts
+++ client/app/front-office/components/session-facilitator/session-facilitator.component.ts
@@ -1,4 +1,13 @@
 import {Component, OnInit} from '@angular/core';
+import {NaturalQueryVariablesManager} from '@ecodev/natural';
+import {UserService} from '../../../admin/users/services/user.service';
+import {
+    SortingOrder,
+    UserRole,
+    Users_users_items,
+    UserSortingField,
+    UsersVariables,
+} from '../../../shared/generated-types';
 
 @Component({
     selector: 'app-session-facilitator',
@@ -6,7 +15,18 @@ import {Component, OnInit} from '@angular/core';
     styleUrls: ['./session-facilitator.component.scss'],
 })
 export class SessionFacilitatorComponent implements OnInit {
-    constructor() {}
+    public facilitators: Users_users_items[] = [];
 
-    ngOnInit(): void {}
+    constructor(public userService: UserService) {}
+
+    ngOnInit(): void {
+        const qvm = new NaturalQueryVariablesManager<UsersVariables>();
+        qvm.set('variables', {
+            filter: {groups: [{conditions: [{role: {in: {values: [UserRole.facilitator]}}}]}]},
+            pagination: {pageSize: 999, pageIndex: 0},
+            sorting: [{field: UserSortingField.locality, order: SortingOrder.ASC}],
+        });
+
+        this.userService.getAll(qvm).subscribe(result => (this.facilitators = result.items));
+    }
 }

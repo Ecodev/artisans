ada16f0c8c5962f3898e64b2b00536a1358ee3b3
Adrien Crivelli <adrien.crivelli@gmail.com>
2020-11-11 18:33:11 +0900

    Check all kind of files, not only images #7591

--- bin/check-files.php
+++ bin/check-files.php
@@ -6,26 +6,14 @@
  *
  * It is up to the user to then take appropriate action based on that information.
  */
-use Application\Model\Image;
-use Application\Repository\ImageRepository;
-use Ecodev\Felix\Utility;
+
+use Ecodev\Felix\Service\FileChecker;
 
 require_once 'server/cli.php';
 
-/** @var ImageRepository $imageRepository */
-$imageRepository = _em()->getRepository(Image::class);
-$filesInDb = $imageRepository->getFilenames();
-$filesOnDisk = glob('data/images/*');
+$checker = new FileChecker(_em()->getConnection());
 
-$missingFiles = array_diff($filesInDb, $filesOnDisk);
-$unneededFiles = array_diff($filesOnDisk, $filesInDb);
-
-Utility::printFiles('List of missing files on disk:', $missingFiles);
-Utility::printFiles('List of unneeded files on disk:', $unneededFiles);
-
-echo '
-Total files in DB     : ' . count($filesInDb) . '
-Total files on disk   : ' . count($filesOnDisk) . '
-Missing files on disk : ' . count($missingFiles) . '
-Unneeded files on disk: ' . count($unneededFiles) . '
-';
+$checker->check([
+    'image' => 'data/images/',
+    'file' => 'data/file/',
+]);

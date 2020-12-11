f22ce5f00e79211961b5133ce6e306a74698f564
Samuel Baptista <samuel.baptista@ecodev.ch>
2020-12-11 16:49:14 +0100

    Redirect empty page

--- client/app/front-office/modules/shop/shop-routing.module.ts
+++ client/app/front-office/modules/shop/shop-routing.module.ts
@@ -13,7 +13,7 @@ const routes: Routes = [
     {
         path: '',
         pathMatch: 'full',
-        redirectTo: 'articles',
+        redirectTo: '/larevudurable/articles',
     },
     {
         path: 'recherche',

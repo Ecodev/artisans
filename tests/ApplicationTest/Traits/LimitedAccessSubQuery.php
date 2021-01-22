69c8a55b73b93508334e7e1f606fb61130daa40f
Adrien Crivelli <adrien.crivelli@gmail.com>
2021-01-22 10:48:22 +0900

    Declare required methods as abstract

--- tests/ApplicationTest/Traits/LimitedAccessSubQuery.php
+++ tests/ApplicationTest/Traits/LimitedAccessSubQuery.php
@@ -6,6 +6,7 @@ namespace ApplicationTest\Traits;
 
 use Application\Model\User;
 use Application\Repository\UserRepository;
+use Doctrine\ORM\EntityManager;
 use PDO;
 
 /**
@@ -13,19 +14,21 @@ use PDO;
  */
 trait LimitedAccessSubQuery
 {
+    abstract public function getEntityManager(): EntityManager;
+
     /**
      * @dataProvider providerGetAccessibleSubQuery
      */
     public function testGetAccessibleSubQuery(?string $login, array $expected): void
     {
         /** @var UserRepository $userRepository */
-        $userRepository = _em()->getRepository(User::class);
+        $userRepository = $this->getEntityManager()->getRepository(User::class);
         $user = $userRepository->getOneByEmail($login . '@example.com');
         $subQuery = $this->repository->getAccessibleSubQuery($user);
         if ($subQuery === '-1') {
             $ids = [];
         } else {
-            $ids = _em()->getConnection()->executeQuery($subQuery)->fetchAll(PDO::FETCH_COLUMN);
+            $ids = $this->getEntityManager()->getConnection()->executeQuery($subQuery)->fetchAll(PDO::FETCH_COLUMN);
         }
 
         sort($ids);

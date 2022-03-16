f492696319c2d12a0ed0f778244a6dfd4d937b13
Adrien Crivelli <adrien.crivelli@gmail.com>
2022-03-16 10:35:16 +0100

    Test that schema can be introspected

--- tests/ApplicationTest/Api/SchemaTest.php
+++ tests/ApplicationTest/Api/SchemaTest.php
@@ -5,15 +5,34 @@ declare(strict_types=1);
 namespace ApplicationTest\Api;
 
 use Application\Api\Schema;
+use GraphQL\GraphQL;
+use GraphQL\Type\Introspection;
 use PHPUnit\Framework\TestCase;
 
 class SchemaTest extends TestCase
 {
+    private Schema $schema;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->schema = new Schema();
+    }
+
     public function testSchemaIsValid(): void
     {
-        $schema = new Schema();
-        $schema->assertValid();
+        $this->schema->assertValid();
 
         self::assertTrue(true, 'schema passes validation');
+    }
+
+    public function testSchemaCanBeIntrospected(): void
+    {
+        $result = GraphQL::executeQuery(
+            $this->schema,
+            Introspection::getIntrospectionQuery()
+        );
+
+        self::assertSame([], $result->errors, 'should not have any errors during introspection');
     }
 }

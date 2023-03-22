d3d95bf34b3af44ec6e5e56d8b389501675723ca
Adrien Crivelli <adrien.crivelli@gmail.com>
2023-03-22 10:52:33 +0700

    Drop dead code #9432

--- client/app/front-office/components/session-method/session-method.component.ts
+++ client/app/front-office/components/session-method/session-method.component.ts
@@ -2,7 +2,7 @@ import {Component} from '@angular/core';
 
 @Component({
     selector: 'app-session-method',
-    templateUrl: './session-method-2.component.html',
+    templateUrl: './session-method.component.html',
     styleUrls: ['./session-method.component.scss'],
 })
 export class SessionMethodComponent {}

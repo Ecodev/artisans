5b9e795cb6ab9313fbc44bf0daec95343ad23c25
Adrien Crivelli <adrien.crivelli@gmail.com>
2023-11-11 17:45:53 +0900

    Drop JS coverage that we never used and use obsolete packages #9816

--- client/karma.conf.js
+++ client/karma.conf.js
@@ -12,22 +12,15 @@ module.exports = function (config) {
             require('karma-jasmine'),
             require('karma-chrome-launcher'),
             require('karma-jasmine-html-reporter'),
-            require('karma-coverage-istanbul-reporter'),
             require('@angular-devkit/build-angular/plugins/karma'),
         ],
         client: {
             clearContext: false, // leave Jasmine Spec Runner output visible in browser
         },
-        coverageIstanbulReporter: {
-            dir: require('node:path').join(__dirname, '../coverage'),
-            reports: ['html', 'lcovonly'],
-            fixWebpackSourcePaths: true,
+        jasmineHtmlReporter: {
+            suppressAll: true, // removes the duplicated traces
         },
         reporters: ['progress', 'kjhtml'],
-        port: 9876,
-        colors: true,
-        logLevel: config.LOG_INFO,
-        autoWatch: true,
         browsers: ['Chrome'],
         customLaunchers: {
             ChromeHeadlessCustom: {
@@ -35,7 +28,7 @@ module.exports = function (config) {
                 flags: ['--no-sandbox'],
             },
         },
-        singleRun: false,
         failOnSkippedTests: true,
+        restartOnFileChange: true,
     });
 };

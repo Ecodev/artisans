b31f55be293f12bebeb3b18556bbfaf6ca3fdde2
Adrien Crivelli <adrien.crivelli@gmail.com>
2024-09-29 17:02:25 +0200

    Wait for more semantic queries #10746
    It shouldn't change anything in practice, but it is semantically more
    correct to wait for viewer and login mutation

--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,24 +1,23 @@
 import {formControlName, graphqlRequest} from './utils';
 import {Page} from '@playwright/test';
-import {permissionsQuery} from '../client/app/shared/services/permissions.queries';
+import {currentUserForProfileQuery, loginMutation} from '../client/app/admin/users/services/user.queries';
 
 export class AppPage {
     public constructor(private readonly page: Page) {}
 
     public async login(login: string): Promise<unknown> {
         await Promise.all([
-            // Login page will load for permissions, so we need to be sure we finish that before reseting Apollo store when login
-            this.page.waitForResponse(graphqlRequest(permissionsQuery)),
+            // Login page will load viewer, so we need to be sure we finish that before resetting Apollo store when login
+            this.page.waitForResponse(graphqlRequest(currentUserForProfileQuery)),
             this.page.goto('login?logout=true'),
         ]);
 
         await this.page.type(formControlName('login'), login);
         await this.page.type(formControlName('password'), login);
 
-        // Alternative way with a predicate.
         return await Promise.all([
-            // Wait for the permissions to be fetched with the new user
-            this.page.waitForResponse(graphqlRequest(permissionsQuery)),
+            // Wait for the mutation to be fetched with the new user
+            this.page.waitForResponse(graphqlRequest(loginMutation)),
 
             // Triggers the login
             this.page.click('button[type="submit"]'),

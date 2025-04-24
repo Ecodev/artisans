c0026aef9bc735dd06686636d16948e7f2292e26
Adrien Crivelli <adrien.crivelli@gmail.com>
2025-04-24 16:16:44 +0900

    All unit tests pass #10570

--- server/Application/Log/DbWriterFactory.php
+++ server/Application/Log/DbWriterFactory.php
@@ -15,9 +15,11 @@ class DbWriterFactory implements FactoryInterface
 {
     public function __invoke(ContainerInterface $container, $requestedName, ?array $options = null)
     {
-        $logRepository = $container->get(EntityManager::class)->getRepository(Log::class);
         $eventCompleter = $container->get(EventCompleter::class);
 
-        return new Db($logRepository, $eventCompleter);
+        return new Db(
+            fn () => $container->get(EntityManager::class)->getRepository(Log::class),
+            $eventCompleter
+        );
     }
 }

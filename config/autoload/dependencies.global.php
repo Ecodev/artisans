fe55e713acb8b353d8016d0f90891c7b481f8eb7
Adrien Crivelli <adrien.crivelli@gmail.com>
2025-05-12 16:14:16 +0900

    Prevent `doctrine_migration_versions` to be shown as non-sync error #11390
    `./bin/doctrine orm:validate-schema` and `./bin/doctrine
    orm:schema-tool:update --dump-sql` both incorrectly showed the DB
    structure was out of sync because of the existence of the table
    `doctrine_migration_versions`. The table must exist, and it must never
    be deleted.
    
    We reproduce the behavior of Doctrine older versions to ignore that
    specific table for those two specific commands.
    
    This also happens to create a base for a future Felix-dedicated CLI with
    some commands defined within Felix and others within the application.

--- config/autoload/dependencies.global.php
+++ config/autoload/dependencies.global.php
@@ -7,11 +7,6 @@ return [
     // We recommend using fully-qualified class names whenever possible as
     // service names.
     'dependencies' => [
-        // Use 'aliases' to alias a service name to another service. The
-        // key is the alias name, the value is the service to which it points.
-        'aliases' => [
-            Doctrine\ORM\EntityManager::class => 'doctrine.entity_manager.orm_default',
-        ],
         // Use 'invokables' for constructor-less services, or services that do
         // not require arguments to the constructor. Map a service name to the
         // class name.
@@ -23,8 +18,6 @@ return [
         // Use 'factories' for services provided by callbacks/factory classes.
         'factories' => [
             'doctrine.entity_manager.orm_default' => Application\ORM\EntityManagerFactory::class,
-            Doctrine\Migrations\Configuration\Migration\ConfigurationLoader::class => Roave\PsrContainerDoctrine\Migrations\ConfigurationLoaderFactory::class,
-            Doctrine\Migrations\DependencyFactory::class => Roave\PsrContainerDoctrine\Migrations\DependencyFactoryFactory::class,
             Application\Handler\DatatransHandler::class => Application\Handler\DatatransFactory::class,
             Application\Middleware\AuthenticationMiddleware::class => Application\Middleware\AuthenticationFactory::class,
             Application\Service\Invoicer::class => Application\Service\InvoicerFactory::class,

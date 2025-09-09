ed509f6c330b5b3b4028950d093a73c9b8dfa899
Adrien Crivelli <adrien.crivelli@gmail.com>
2025-09-09 22:46:39 +0900

    Drop unused components

--- client/app/front-office/components/agir-avec-nous/apero-divest/apero-divest.component.ts
+++
@@ -1,9 +0,0 @@
-import {Component} from '@angular/core';
-
-@Component({
-    selector: 'app-desinvestir-fossile',
-    standalone: true,
-    templateUrl: './apero-divest.component.html',
-    styleUrl: './apero-divest.component.scss',
-})
-export class AperoDivestComponent {}
--- client/app/front-office/modules/shop/components/subscribe/subscribe.component.ts
+++
@@ -1,9 +0,0 @@
-import {Component} from '@angular/core';
-
-@Component({
-    selector: 'app-subscribe',
-    standalone: true,
-    templateUrl: './subscribe.component.html',
-    styleUrl: './subscribe.component.scss',
-})
-export class SubscribeComponent {}

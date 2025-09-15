fafd1d2eed79781d4099e0b4f1d9bae3bcfc1794
Samuel Baptista <samuel.baptista@ecodev.ch>
2025-09-15 16:12:23 +0200

    Content update : add review number in text page #11787

--- client/app/front-office/components/agir-avec-nous/irremplacables-epiceries/irremplacables-epiceries.component.ts
+++ client/app/front-office/components/agir-avec-nous/irremplacables-epiceries/irremplacables-epiceries.component.ts
@@ -1,8 +1,10 @@
 import {Component} from '@angular/core';
+import {NaturalSrcDensityDirective} from '@ecodev/natural';
+import {RouterLink} from '@angular/router';
 
 @Component({
     selector: 'app-irremplacables-epiceries',
-    imports: [],
+    imports: [NaturalSrcDensityDirective, RouterLink],
     templateUrl: './irremplacables-epiceries.component.html',
     styleUrl: './irremplacables-epiceries.component.scss',
 })

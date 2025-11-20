fd8e65198cf7b1f86cbc6ac8a7a715216569f589
Samuel Baptista <samuel.baptista@ecodev.ch>
2025-11-20 02:35:52 +0100

    MD3 migration #11936

--- client/app/shared/components/boot-loader/boot-loader.component.ts
+++ client/app/shared/components/boot-loader/boot-loader.component.ts
@@ -3,5 +3,6 @@ import {Component} from '@angular/core';
 @Component({
     selector: 'app-boot-loader',
     templateUrl: './boot-loader.component.html',
+    styleUrl: 'boot-loader.component.scss',
 })
 export class BootLoaderComponent {}

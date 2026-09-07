33374f36a09df4778837ddb293dbfdf7442f836e
Adrien Crivelli <adrien.crivelli@gmail.com>
2026-09-07 13:35:27 +0900

    pnpm v12 `--frozen-lockfile` should be faster and as much correct as pnpm v11 `ci`

--- bin/build.sh
+++ bin/build.sh
@@ -68,7 +68,7 @@ echo "Installing git hooks..."
 ln -fs ../../bin/pre-commit.sh .git/hooks/pre-commit
 
 echo "Updating Node.js packages..."
-pnpm ci
+pnpm install --frozen-lockfile
 
 echo "Updating all PHP dependencies via composer..."
 $COMPOSER install --classmap-authoritative $NO_PROGRESS

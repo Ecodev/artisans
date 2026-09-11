1cd9466704a97ad76af85593b45832f70b86457f
Adrien Crivelli <adrien.crivelli@gmail.com>
2026-09-11 16:06:33 +0900

    Support direct access with HTTP AUTH: https://user:pass@example.com

--- client/app/shared/config/apollo-options.provider.ts
+++ client/app/shared/config/apollo-options.provider.ts
@@ -38,7 +38,7 @@ function createApolloLink(
 
     return errorLink.concat(
         createHttpLink(httpLink, httpBatchLink, {
-            uri: '/graphql?v=' + localConfig.version,
+            uri: window.location.origin + '/graphql?v=' + localConfig.version,
         }),
     );
 }
